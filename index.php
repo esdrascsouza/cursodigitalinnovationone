7c4f531660917c8fc7bb72ad2ff72408128d2b9e
esdrascsouza <esdrasbarba@yahoo.com.br>
2019-12-31 10:35:29 -0300

    formulários com php

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+echo"Hello Word";
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+   <head>
+   	 <meta charset="utf-8"/>
+       <title>Formulario com php</title>
+         <link rel="stylesheet" type="text/css" href="css/style.css"/>
+   </head>
+      <body>
+      	  <form action="#" method="post">
+             <fieldset>
+             	<legend>Dados Mergulhador</legend>
+                   <labe>Nome</labe>
+                     <input type="text" name="nome" id="nome"/>
+                       <label>Idade</label>
+                         <input type="text" name="idade" id="idade"/>
+                           </fieldset>
+                         <button id="btn" type="submit">Send</button>
+      	  </form>
+
+      </body>
+
+
+</html>
